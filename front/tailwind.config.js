338dea0dc5820f4f51854ee556d98daed94c58a8
Michael <gt.gluck@gmail.com>
2023-09-15 20:38:32 +0300

    add: tailwind tag

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -8,11 +8,19 @@ export default {
       },
       colors: {
         theme: {
-          green: '#5CB85C'
+          green: '#5CB85C',
+          darkGreen: '#3d8b3d',
+          lightenGray: '#ddd',
+          gray: '#bbb',
+          darkestGray: '#373a3c',
+          darkenGray: '#999',
+          tag: '#aaa'
         },
       },
       spacing: {
-        navItem: '0.425rem'
+        navItem: '0.425rem',
+        tag: '0.6rem',
+        '0.3': '0.3rem'
       },
       boxShadow: {
         banner: 'rgba(0, 0, 0, 0.3) 0px 1px 3px'
@@ -21,7 +29,12 @@ export default {
         logo: '0px 1px 3px rgba(0, 0, 0, 0.3)'
       },
       fontSize: {
-        logo: '3.5rem'
+        logo: '3.5rem',
+        date: '0.8rem',
+      },
+      borderRadius: {
+        buttonSm: '0.2rem',
+        tag: '10rem',
       }
     },
   },
